afba54c8013de0c7d669f04a3460c8c988b9256e
Quoc Huy <nhquochuy@outlook.com>
2020-05-22 07:09:12 +0700

    Add project files.

---
+++ PracticeShop.Data/Services/UserData.cs
@@ -0,0 +1,22 @@
+﻿using PracticeShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeShop.Data.Services
+{
+    public class UserData : IUserData
+    {
+        PracticeShopDbContext db = null;
+        public UserData()
+        {
+            this.db = new PracticeShopDbContext();
+        }
+        public User Login(string username, string password)
+        {
+            return db.Users.FirstOrDefault(x => x.UserName == username && x.Password == password);
+        }
+    }
+}
---
+++ PracticeShop.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using PracticeShop.Data.Services;
+using PracticeShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeShop.Web.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return HttpNotFound();
+        }
+
+        [HttpGet]
+        public ActionResult Login(UserModel userModel)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel userModel, FormCollection form)
+        {
+            IUserData userData = new UserData();
+            if (ModelState.IsValid)
+            {
+                var re = userData.Login(userModel.UserName, userModel.Password);
+                if (re != null)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+                else TempData["wronguser"] = "Login Fail !!!";
+            }
+            return View();
+        }
+    }
+}
